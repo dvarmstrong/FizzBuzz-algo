03980a39dea0f299bc5b2621b0169d4ff0c2a0cd
DVA <61880632+dvarmstrong@users.noreply.github.com>
2021-10-21 13:26:58 -0400

    The first of my Algo Challenges

---
+++ Fizzbuzz algo.js
@@ -0,0 +1,6 @@
+// Fizz buzz Challenge
+// For each number that is a multiple of 3 print "Fizz"
+// For each number that is a multiple of 3 print "Buzz"
+// For numbers wqhich are a multiple of both 3 and 5 print "Fizzbuzz"
+// All Numbers should print 
+
